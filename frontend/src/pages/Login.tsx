0efad7589ca74e8f149fff4e7342e9f8fe78572b
Aline Costa <alinecosta@tecsus.com.br>
2023-04-23 21:28:28 -0300

    Integração backend e frontend login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,19 +1,59 @@
+import { FormEvent, useRef } from 'react';
 import './Login.css';
 
 function Login() {
+  const email = useRef<HTMLInputElement>(null);
+  const password = useRef<HTMLInputElement>(null);
+
+  async function authenticate(e: FormEvent<HTMLFormElement>){
+    e.preventDefault();
+
+    const userEmail = email.current?.value || "";
+    const userPassword = password.current?.value || "";
+
+    const response = await fetch('http://localhost:3000/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: userEmail,
+        password_user: userPassword,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'Access-Control-Allow-Methods': 'POST, GET, PUT, PATCH, DELETE'
+      },
+      
+    });
+
+      const data = await response.json()
+      if(response.ok){
+        const token = data.token
+        localStorage.setItem('token', token)
+        alert('Autenticado')
+      }else{
+        alert('Autenticação inválida')
+      }
+      console.table(data)
+  }
+
   return (
     <>
       <p id='login'>Inspeção de Entrada de Produtos</p><br/>
       <div className="infoConta">
-          <label className='infoContaName'>E-mail</label>
+      <form onSubmit={authenticate}>
+        <label className='infoContaName'>Email
           <br/>
-          <input type="email" id="email" name="email" required />
-          <br/><br/>
-          <label className='infoContaName'>Senha</label>
+          <input type="email" ref = {email} required />
+        </label>
+        <br/><br/>
+        <label className='infoContaName'>Senha
           <br/>
-          <input type="password" name="senhaLogin" />
-          <br/><br/>
-          <input type="submit" className="entrar" value="Entrar" />
+          <input type="password" ref = {password} />
+        </label>
+        <br/><br/>
+        <button type="submit" className="entrar">Login</button>
+      </form>
       </div>
     </>
   );
